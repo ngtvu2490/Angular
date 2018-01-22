bff3803339697d653bc22a8b9538ff5aa91f959f
ngtvu2490 <nguyentrongvu90@gmail.com>
2018-01-22 22:42:57 +0700

    Basic service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,7 +10,7 @@ import 'hammerjs';
 import { AppComponent } from './app.component';
 import { MenuComponent } from './menu/menu.component';
 import { DishdetailComponent } from './dishdetail/dishdetail.component';
-
+import { DishService } from './services/dish.service';
 
 @NgModule({
   declarations: [
@@ -26,7 +26,7 @@ import { DishdetailComponent } from './dishdetail/dishdetail.component';
     MaterialModule,
     FlexLayoutModule
   ],
-  providers: [],
+  providers: [DishService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
